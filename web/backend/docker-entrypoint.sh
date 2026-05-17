4197a8e38ef4afe856446e46529b688efb9b9732
BrazhenkoOleg <brazhenko.oleg@yandex.ru>
2026-05-18 00:51:33 +0300

    Updated Dockerfile

--- web/backend/docker-entrypoint.sh
+++ web/backend/docker-entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 set -e
 
+echo "Проверяем Laravel public/index.php..."
+ls -la /var/www/html/public
+test -f /var/www/html/public/index.php
+
 echo "Очищаем Laravel cache..."
 rm -rf bootstrap/cache/*.php
 
-echo "Очищаем runtime cache..."
 php artisan config:clear
 php artisan view:clear
 php artisan route:clear
@@ -19,6 +22,9 @@ echo "Кэшируем production config..."
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
+
+echo "Показываем маршруты..."
+php artisan route:list
 
 echo "Запуск Apache..."
 exec apache2-foreground
